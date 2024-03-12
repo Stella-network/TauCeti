1164f1940f1ee998c63ac4a0d2869070c188f8c4
A.Genno <akiyuki.genno@gmail.com>
2024-03-13 03:56:41 +0900

    Vue3インストール

---
+++ project/resources/js/sample/Sample.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('Laravel with Vue3')
+const message = ref('This is a sample component using Vue3 with Laravel. this message is a constant of JavaScript in resources/js/sample/Sample.vue')
+</script>
+
+<template>
+  <center>
+    <h1 class="d-flex w-100">{{ title }}</h1>
+    <p>
+      {{ message }}
+    </p>
+  </center>
+</template>
---
+++ project/resources/js/sample/sample.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+
+import App from "./Sample.vue";
+
+const app = createApp(App);
+
+app.mount("#sample-is-here");
